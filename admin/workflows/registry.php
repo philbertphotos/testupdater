263a890fa876479a8e544b7fe3201be70d3f3163
Joseph Philbert <joe@philbertphotos.com>
2026-08-21 01:40:25 -0400

    Add files via upload

--- admin/workflows/registry.php
+++ admin/workflows/registry.php
@@ -17,12 +17,15 @@ require_once(DOCROOT . '/admin/workflows/classes/actioninterface.php');
 require_once(DOCROOT . '/admin/workflows/classes/engine.php');
 
 return array(
+	'security.clear.attempts'        => 'Action_ClearAttempts',
+	'security.cleanup.verification'  => 'Action_VerificationCleanup',
+	'security.reset.lockouts'        => 'Action_ResetLoginCount',
+	'system.update.auto' => 'Action_AutoUpdate',
 	'email.send' => 'Action_SendEmail',
 	'users.find.new' => 'Action_FindNewUsers',
     'report_logs' => 'Action_ReportLogs',
     'email_context' => 'Action_EmailContext',
 	'cleanup_old_sessions' => 'Action_CleanupOldSessions',
     'test.echo' => 'Action_TestEcho'
-
 );
 ?>
